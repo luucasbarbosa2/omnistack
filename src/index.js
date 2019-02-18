095894bf4e6f07b63f81faeffcbca4fb6d426364
lucasbarbosa <lucasbarbosa@IFSPSAOCARLOS.EDU.BR>
2019-02-18 16:40:34 -0300

    inicializando

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+
+
+mongoose.connect('mongodb://lucas:123456789lu@ds245234.mlab.com:45234/goweek-backend-lucas', {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next)=>{
+    req.io = io;
+    return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
+
+
+
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+
+routes.get('/tweets', TweetController.index); 
+routes.post('/tweets', TweetController.store); 
+
+routes.post('/likes/:id', LikeController.store); 
+
+
+routes.get('/', (req, res) => {
+    return res.send('Helloo World')
+});
+
+
+
+module.exports = routes;
